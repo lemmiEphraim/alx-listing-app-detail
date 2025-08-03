cded4853e5544bd2c81194a8d6287157e9de2ab4
lemmiEphraim <lemmiabdissa@gmail.com>
2025-08-04 00:45:54 +0300

    Update index.ts

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,1 +1,20 @@
-
+export interface PropertyProps {
+  id: string;
+  name: string;
+  description: string;
+  rating: number;
+  price: number;
+  image: string;
+  images: string[];
+  category: string[];
+  reviews: {
+    name: string;
+    avatar: string;
+    rating: number;
+    comment: string;
+  }[];
+  address: {
+    city: string;
+    country: string;
+  };
+}
